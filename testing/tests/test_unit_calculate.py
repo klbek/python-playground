5c6f6b7e6a6642c36acf42e45983fc25acce288e
KlaraBek <sadecka.kl@gmail.com>
2025-01-18 23:49:05 +0100

    unit_test is_even

--- testing/tests/test_unit_calculate.py
+++ testing/tests/test_unit_calculate.py
@@ -1,12 +1,21 @@
 import unittest
 from files import calculate as c
 
-class TestAddFunction(unittest.TestCase):
+class TestCalcFunction(unittest.TestCase):
     def test_add_numbers(self):
         self.assertEqual(c.add_numbers(4, 8), 12)
         self.assertEqual(c.add_numbers(-2, 2), 0)
         self.assertEqual(c.add_numbers(4, -10), -6)
 
+    def test_is_even(self):
+        self.assertTrue(c.is_even(4))
+        self.assertTrue(c.is_even(0))
+        self.assertTrue(c.is_even(-6))
+        self.assertFalse(c.is_even(-1))
+        self.assertFalse(c.is_even(-3))
+
+
+# pro spousteni pomoci pytest neni nutne
 if __name__ == "__main__":
     unittest.main()
 
