2ab907583ce4bf93db35f83840aa0297988e0c94
KlaraBek <sadecka.kl@gmail.com>
2025-01-18 23:32:52 +0100

    test is_even

--- testing/tests/test_pytest_calculate.py
+++ testing/tests/test_pytest_calculate.py
@@ -4,3 +4,11 @@ def test_add_function():
     assert c.add_numbers(4, 8) == 12
     assert c.add_numbers(-2, 2) == 0
     assert c.add_numbers(4, -10) == -6
+
+def test_is_even():
+    assert c.is_even(4) is True
+    assert c.is_even(0) is True
+    assert c.is_even(-6) is True
+    assert c.is_even(-1) is False
+    assert c.is_even(3) is False
+    
